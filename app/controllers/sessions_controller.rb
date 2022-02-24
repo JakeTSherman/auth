a39d3ffcb0fbe5cab219bd3db803d20adbcdcec6
JakeTSherman <jake.sherman@kellogg.northwestern.edu>
2022-02-24 01:56:42 +0000

    encrypt passwords

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,13 +6,16 @@ class SessionsController < ApplicationController
     def create
          @user = User.where({email: params["email"]})[0]
         if @user
-            if @user.password == params["password"]
-            flash[:notice] = "Welcome"
-            redirect_to "/companies"
+            if BCrypt::Password.new(@user.password) == params["password"]
+                flash[:notice] = "Welcome"
+                redirect_to "/companies"
             else
+                flash[:notice] = "Nope."
+                redirect_to "/sessions/new"
+            end
+        else
             flash[:notice] = "Nope."
             redirect_to "/sessions/new"
-            end
         end
     end
 end
